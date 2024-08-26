840f5f3d82b43260cd817792bfada8c317bf2e5b
caiocroccia22 <caiocroccia22@gmail.com>
2024-08-25 22:14:30 -0300

    feat: creating updateAverage function

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 const formActivity = document.getElementById('form-activity');
 let linhas = '';
-const imgAprovado = '<img src="img/aprovado.png" alt="Emoji celebrando" />'
-const imgReprovado = '<img src="img/reprovado.png" alt="Emoji triste" />'
+const imgAprovado = '<img src="img/aprovado.png" alt="Emoji celebrando" />';
+const imgReprovado = '<img src="img/reprovado.png" alt="Emoji triste" />';
+const activitys = [];
+const grades = [];
+
 
 formActivity.addEventListener('submit', function(e){
     e.preventDefault();
+    addLine();
+    updateTable();
+    updateAverage();
+});
+
+function addLine(){
     const inputActivityname = document.getElementById('activity-name');
     const inputActivitygrade = document.getElementById('activity-grade');
+
+    activitys.push(inputActivityname.value);
+    grades.push(parseFloat(inputActivitygrade.value));
+
 
     let linha = '<tr>';
     linha = linha + `<td>${inputActivityname.value}</td>`;
@@ -16,15 +29,24 @@ formActivity.addEventListener('submit', function(e){
 
     linhas += linha;
    
-    const tableBody = document.querySelector('tbody');
-    tableBody.innerHTML = linhas;
-
-
-
-    alert(`Atividade: ${inputActivityname.value} - Nota: ${inputActivitygrade.value}`);
-
     inputActivityname.value = '';
     inputActivitygrade.value = '';
+}
 
+function updateTable(){
+    const tableBody = document.querySelector('tbody');
+    tableBody.innerHTML = linhas;
+}
 
-});
+function updateAverage(){
+
+    let soma = 0;
+    for (i = 0; i < grades.length; i++){
+        soma += grades[i];
+    }
+
+    const average = soma / grades.length;
+    console.log(soma);
+    console.log(average)
+    
+}
